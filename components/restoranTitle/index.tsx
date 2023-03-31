bd5f326753ab4852b191945876919167f67a55c4
saidaliyevjasur450@gmail.com <saidaliyevjasur450@gmail.com>
2023-03-31 13:42:28 +0500

    changes

--- components/restoranTitle/index.tsx
+++ components/restoranTitle/index.tsx
@@ -2,35 +2,13 @@ import { FC } from "react";
 //sass
 import styles from "./index.module.sass";
 // carousel nuka
-// import Carousel from "nuka-carousel/lib/carousel";
+import Carousel from "nuka-carousel/lib/carousel";
 // next components
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@mui/material";
-// mui components
-// intl
 import { useTranslations } from "next-intl";
-// carousel 
-import Carousel from "react-multi-carousel";
-
-
-const responsive = {
-  desktop: {
-    breakpoint: { max: 5000, min: 1024 },
-    items: 5,
-    slidesToSlide: 1,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 1,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 2,
-    slidesToSlide: 1,
-  },
-};
+// mui components
 
 type RestoranTitle = {
   subtitle: string;
@@ -60,7 +38,6 @@ export const RestoranTitle: FC<RestoranTitle> = ({
   pdf,
 }) => {
   const t = useTranslations()
-
   return (
     <div className={styles.title}>
       <h3 className={styles.title__subtitle}>{subtitle}</h3>
@@ -68,72 +45,64 @@ export const RestoranTitle: FC<RestoranTitle> = ({
         <h1 className={styles.title__eventText__description}>{description}</h1>
       </div>
       <a className={styles.title__link} href={pdf} rel={"noreferrer"} target={"_blank"}>
-        {t("pageManufacturers.certificate")}
+        Посмотреть сертификат
       </a>
       <h3 className={styles.title__gallery}>Галерея</h3>
 
-      {/* <Carousel
-      className={styles.carousel}
-      swipeable={true}
-      draggable={true}
-      showDots={false}
-      responsive={responsive}
-      ssr={true}
-      infinite={true}
-      rewindWithAnimation={true}
-      autoPlay={true}
-      autoPlaySpeed={4000}
-      keyBoardControl={true}
-      customTransition="all .5"
-      transitionDuration={500}
-      containerClass="carousel-container"
-      dotListClass="custom-dot-list-style"
-      itemClass="carousel-item-padding-40-px"
-      > */}
-      <div className={styles.title__images}>
+      <Carousel
+        autoplay={false}
+        autoplayInterval={7000}
+        animation={"fade"}
+        wrapAround={true}
+        pauseOnHover={true}
+        renderCenterRightControls={null}
+        renderCenterLeftControls={null}
+        renderBottomCenterControls={null}
+        swiping={false}
+      >
         <Image
           className={styles.title__img}
           src={img}
           alt="restoranImages"
-          width={400}
-          height={300}
+          width={1280}
+          height={480}
         />
         <Image
           className={styles.title__img}
           src={img2}
           alt="restoranImages"
-          width={400}
-          height={300}
+          width={1280}
+          height={480}
         />
         <Image
           className={styles.title__img}
           src={img3}
           alt="restoranImages"
-          width={400}
-          height={300}
+          width={1280}
+          height={480}
         />
         <Image
           className={styles.title__img}
           src={img4}
           alt="restoranImages"
-          width={400}
-          height={300}
+          width={1280}
+          height={480}
         />
         <Image
           className={styles.title__img}
           src={img5}
           alt="restoranImages"
-          width={400}
-          height={300}
+          width={1280}
+          height={480}
         />
         <Image
           className={styles.title__img}
           src={img6}
           alt="restoranImages"
-          width={400}
-          height={300}
+          width={1280}
+          height={480}
         />
-      </div>
+      </Carousel>
       <p className={styles.title__social}>Ресторан в соц сетях:</p>
       <div className={styles.title__socialIcons}>
         <a href={instagram} target={"_blank"}>
@@ -154,7 +123,7 @@ export const RestoranTitle: FC<RestoranTitle> = ({
           />
         </a>
       </div>
-      <button className={styles.title__btn}>{t("pageManufacturers.site")}</button>
+      <button className={styles.title__btn}></button>
     </div>
   );
 };
